9e0899e22c52ee941e2c95a147811943ab7be2c0
nickmahd <nickmahda@gmail.com>
2025-08-27 21:17:41 -0400

    party in the USA

--- encoding.py
+++ encoding.py
@@ -1,16 +1,48 @@
-from ring import CR, CRE
-
 import numpy as np
+from ring import CR, CRE
+from util import (
+    sample_hamming_ternary,
+    sample_discrete_gaussian,
+    sample_uniform_cring,
+    sample_zero_one,
+    pad
+)
 
-def encode(input: np.ndarray, D: int, k: int, q: np.ndarray):
-    N = 1 << k
-    L = N >> 1
-    Q = np.prod(q)
+class CKKS:
+    """
+    Cheon-Kim-Kim-Song encryption implementation
 
-    input = np.pad(input, (0, max(0, L - input.size)))[:L]
-    z = np.concatenate((input, np.conjugate(input[::-1])))
+    Parameters
+    ----------
 
-    z_hat = np.fft.ifft(z).real * 2 ** D
-    coeffs = np.rint(z_hat).astype(object)
-    coeffs_pos = (coeffs + Q) % Q
-    return coeffs_pos % q[:, None]
+    level: security parameter (attacks should require Ω(2^λ) ops)
+    q: 
+    L: ~= Multiplicative depth
+    D: Sal
+    """
+    def __init__(self, level: int, q: int, p: int, L: int, Delta_exp: int):
+        self.level = level
+        self.Q = [q * p ** i for i in range(L+1)]
+        self.qL = self.Q[-1]
+        self.M, self.h, self.P, self.sig = self._params_from_security_level(level)
+        self.ring = CR(self.M, self.P * self.qL)
+
+        s = sample_hamming_ternary(self.M, self.h)
+        a = sample_uniform_cring(self.ring)
+        e = sample_discrete_gaussian(self.M, self.sig ** 2)
+        self.sk = (CRE(1, self.ring), CRE(s, self.ring))
+        self.pk = (CRE(a, self.ring), CRE(-a * s + e, self.ring))
+
+        self.Delta = 1 << Delta_exp
+
+
+    def _params_from_security_level(self, level: int) -> tuple[int, int, int, float]:
+        pass
+
+    def encode(self, plaintext: np.ndarray) -> CRE:
+        plaintext = pad(plaintext, self.ring.degree)
+        scaled_proj = self.Delta * np.concatenate((plaintext, np.conjugate(plaintext[::-1])))
+        coords = np.round(scaled_proj).astype(int) % self.qL
+
+
+        return CRE(coords, self.ring)
--- ring.py
+++ ring.py
@@ -1,40 +1,55 @@
 from __future__ import annotations
+from functools import cached_property
+from typing import Generator
 import numpy as np
+from util import pad
 
 class CR:
+    """
+    Cyclotomic ring of degree 2^k, mod an integer.
+    """
     def __init__(self, k: int, mod: int | None = None):
         if not isinstance(k, int) or k < 0:
             raise ValueError("k must be a non-negative integer")
-        
+
         if mod is not None and (not isinstance(mod, int) or mod < 1):
             raise ValueError("mod must be a positive integer")
 
         self.k = k
-        self.n = 2 ** k
+        self.degree = 2 ** k
+        self.n = self.degree * 2 # Roots of unity
         self.mod = mod
-        self.one = CRE([1], self)
         self.zero = CRE([0], self)
-    
+        self.one = CRE([1], self)
+
     def __repr__(self) -> str:
-        mod_R = f"/{self.mod}Z" if self.mod else ""
-        return f"Z[x]{mod_R}/(x^(2^{self.k})+1)"
-    
+        mod_R = f"/{self.mod}ℤ" if self.mod else ""
+        return f"ℤ[x]{mod_R}/(x^(2^{self.k})+1)"
+
     def __eq__(self, other: CR) -> bool:
-        return (self.k, self.n, self.mod) == (other.k, other.n, other.mod)
+        if not isinstance(other, CR):
+            return NotImplemented
+        return (self.k, self.degree, self.mod) == (other.k, other.degree, other.mod)
+
+    def __hash__(self) -> int:
+        return hash((self.k, self.degree, self.mod))
+
+    @cached_property
+    def proots(self) -> np.ndarray:
+        # Exponents of primitive roots of unity
+        return np.array([k for k in range(self.n) if np.gcd(k, self.n) == 1])
 
     def _reduce(self, coeffs: np.ndarray) -> np.ndarray:
-        # Split into initial polynomial of at most deg n-1, and higher terms
-        coeffs, upper = np.split(coeffs, [self.n])
-
-        if upper.size == 0:
-            # Skip reduction and pad lower half if necessary
-            if (size := coeffs.size) < self.n:
-                coeffs = np.pad(coeffs, (0, self.n - size))
+        if coeffs.size <= self.degree:
+            coeffs = pad(coeffs, self.degree)
             return coeffs
+
+        # Split into initial polynomial of at most deg n-1, and higher terms
+        coeffs, upper = np.split(coeffs, [self.degree])
 
         # Since x^n \equiv -1, the signs alternate for segments of length n
         # Track the number of such blocks and the tail
-        blocks, tail = np.divmod(upper.size, self.n)
+        blocks, tail = np.divmod(upper.size, self.degree)
 
         # Handle the tail first
         if tail:
@@ -47,10 +62,10 @@ class CR:
                 return coeffs
         
         # Arrange into rows
-        fold = upper.reshape(-1, self.n)
+        fold = upper.reshape(-1, self.degree)
 
         # Alternating signs in rows
-        signs = ((np.arange(fold.shape[0], int) & 1) * -2 + 1)[:, None]
+        signs = ((np.arange(fold.shape[0], dtype=int) & 1) * -2 + 1)[:, None]
 
         coeffs = coeffs + (signs * fold).sum(axis=0)
         return coeffs
@@ -58,28 +73,61 @@ class CR:
     def reduce(self, coeffs: np.ndarray) -> np.ndarray:
         res = self._reduce(coeffs)
         return res % self.mod if self.mod is not None else res
+    
+    def sample_uniform(self) -> CRE:
+        assert self.mod is not None
+        coeffs = np.random.randint(0, self.mod, size=self.degree)
+        return CRE(coeffs, self)
+
+    def iter_roots(self) -> Generator[complex, None, None]:
+        for j in range(1, self.n):
+            if np.gcd(j, self.n) == 1:
+                yield np.exp(2j * np.pi * j / self.n)   
+
+    def embed(self, coeffs: np.typing.ArrayLike, inv=False) -> np.ndarray:
+        """Canonical embedding R -> H."""
+        if inv:
+            # Expects z in half-complex frequency space (C^n with a symmetry constraint)
+            z_in = np.zeros(self.n, dtype=complex)
+            z_in[self.proots] = coeffs
+            return np.fft.fft(z_in)[:self.degree] / self.degree
+        else:
+            # Canonical embedding
+            coeffs = self._reduce(coeffs)
+            coeffs = pad(coeffs, self.n)
+            return np.conj(np.fft.fft(coeffs)[self.proots])
+
+    def encode(self, z: np.ndarray, Delta_exp: int) -> np.ndarray:
+        assert z.shape == (self.degree // 2,)
+        z_proj = 10 ** Delta_exp * np.concatenate((z, np.conj(z[::-1])))
+        coeffs = np.round(self.embed(z_proj, inv=True))
+        return CRE(coeffs.real.astype(int), self)
+
 
 class CRE:
+    """
+    Element of cyclotomic ring.
+    """
     def __init__(self, coeffs: np.typing.ArrayLike, ring: CR, asc_deg: bool = True):
         # TODO enforce integer coeffs
-
         self.ring = ring
-        self.coeffs = np.array(coeffs if asc_deg else reversed(coeffs))
+        self.coeffs = np.array(coeffs if asc_deg else coeffs[::-1])
         self.coeffs = self.ring.reduce(self.coeffs)
-
-    @classmethod
-    def from_roots(cls, roots: np.ndarray, ring: CR) -> CRE:
-        res = ring.one
-        for root in roots:
-            res *= CRE([-root, 1], ring)
-        return res
+    
+    def __hash__(self) -> int:
+        return hash((tuple(self.coeffs), self.ring))
 
     def __add__(self, other: CRE) -> CRE:
-        assert self.ring == other.ring
+        if self.ring != other.ring:
+            raise TypeError("Cannot add elements from different rings")
+
         result = self.coeffs + other.coeffs
         return self.__class__(result, self.ring)
     
     def __sub__(self, other: CRE) -> CRE:
+        if self.ring != other.ring:
+            raise TypeError("Cannot subtract elements from different rings")
+
         return self + -1 * other
     
     def __neg__(self) -> CRE:
@@ -88,29 +136,46 @@ class CRE:
     def __mul__(self, other: CRE | int) -> CRE:
         if isinstance(other, int):
             return self.__class__(other * self.coeffs, self.ring)
-        assert self.ring == other.ring
+        if self.ring != other.ring:
+            raise TypeError("Cannot multiply elements from different rings")
         result = np.convolve(self.coeffs, other.coeffs)
         return self.__class__(result, self.ring)
     
-    def __rmul__(self, other: CRE | int) -> CRE:
+    def __rmul__(self, other: int) -> CRE:
         return self.__mul__(other)
-    
-    def __pow__(self, pow: int) -> CRE:
+
+    def __pow__(self, exp: int) -> CRE:
+        if exp < 0:
+            raise ValueError("Negative exponents not supported")
+
+        if exp == 0:
+            return self.ring.one
+        if exp == 1:
+            return self
+
         res = self.ring.one
         base = self
-        if pow == 0:
-            return res
-        while pow > 0:
-            if pow & 1:
+
+        while exp > 0:
+            if exp & 1:
                 res = res * base
             base = base * base
-            pow >>= 1
+            exp >>= 1
+
         return res
 
-    def __call__(self):
-        raise NotImplementedError("Use as ring element, not functions Z -> Z.")
+    def __call__(self, x):
+        if isinstance(x, CRE):
+            res = self.ring.zero
+        else:
+            res = 0
+        for pow, coeff in enumerate(self.coeffs):
+            res += coeff * x ** pow
+        return res
 
     def __eq__(self, other: CRE) -> bool:
+        if not isinstance(other, CRE):
+            return NotImplemented
         return np.array_equal(self.coeffs, other.coeffs) and self.ring == other.ring
 
     def __repr__(self) -> str:
@@ -121,7 +186,18 @@ class CRE:
             if i == 0:
                 terms.append(str(coeff))
             else:
-                coeff_s = ("- " if coeff < 0 else "+ ") + (str(abs(coeff)) if abs(coeff) != 1 else "")
+                prefix = ("- " if coeff < 0 else "+ ") if terms else ""
+                coeff_s = prefix + (str(abs(coeff)) if abs(coeff) != 1 else "")
                 pow_s = f"^{i}" if i > 1 else ""
                 terms.append(f"{coeff_s}x{pow_s}")
-        return f"{' '.join(terms)} \\in {self.ring}"
+        return f"{' '.join(terms)} ∈ {self.ring}"
+
+    def decode(self, Delta_exp: int) -> np.ndarray:
+        scaled_coeffs = self.coeffs / 10 ** Delta_exp
+        left, right = np.split(self.ring.embed(scaled_coeffs), [self.ring.degree // 2])
+        assert np.allclose(left, np.conjugate(right[::-1]))
+        return left
+
+    def degree(self) -> int:
+        terms = self.coeffs.nonzero()[0]
+        return terms[-1] if terms.size > 0 else 0
